092472cec76e9768908ac930ca9ec028168d0745
sharangabo <e.sharangab@alustudent.com>
2024-06-12 12:37:48 +0200

    unitest

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -12,8 +12,7 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-
-     @parameterized.expand([
+    @parameterized.expand([
         ({}, ("a",), 'KeyError: "a"'),
         ({"a": 1}, ("a", "b"), 'KeyError: "b"')
     ])
