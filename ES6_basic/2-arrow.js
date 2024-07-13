a91286cbce54bd934086248ad0efaf8a48d08db9
sharangabo <e.sharangab@alustudent.com>
2024-07-13 16:04:08 +0200

    passes lint

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,6 +1,6 @@
 export default function getNeughborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-  
+
   const self = this;
   this.addNeighborhood = (newNeighborhood) => {
     self.sanFranciscoNeighborhoods.push(newNeighborhood);
