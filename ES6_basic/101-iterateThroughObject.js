364d8de2e5f3e69ca458c8c18f70087afc3857ef
sharangabo <e.sharangab@alustudent.com>
2024-07-15 10:59:06 +0200

    await

---
+++ ES6_basic/101-iterateThroughObject.js
@@ -0,0 +1,10 @@
+const employees = {
+    ...createEmployeesObject('engineering', ['Bob', 'Jane']),
+    ...createEmployeesObject('marketing', ['Sylvie'])
+  };
+  
+  const report = createReportObject(employees);
+  const reportWithIterator = createIteratorObject(report);
+  
+  console.log(iterateThroughObject(reportWithIterator));
+  // Output: Bob | Jane | Sylvie
