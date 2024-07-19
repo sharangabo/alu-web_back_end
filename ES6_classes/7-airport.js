c8576f70dada75dd65eaf68137daabdffa33c0c9
sharangabo <e.sharangab@alustudent.com>
2024-07-20 00:08:23 +0200

    classes

--- ES6_classes/7-airport.js
+++ ES6_classes/7-airport.js
@@ -2,9 +2,10 @@ export default class Airport {
   constructor(name, code) {
     this._name = name;
     this._code = code;
-  }
+    }
 
   toString() {
     return `[object ${this._code}]`;
   }
 }
+  
